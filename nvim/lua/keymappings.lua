c3e84ce4c8a1929f658a1fd2d2a992c594ed7554
Michael Scott <michael.yates.scott@gmail.com>
2022-12-13 19:36:53 -0600

    Add window keymappings for nvim

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -3,4 +3,9 @@ vim.keymap.set('n', '-v', ':Vexplore<Enter>', { silent = true })
 vim.keymap.set('n', '-s', ':Sexplore<Enter>', { silent = true })
 vim.keymap.set('n', '-t', ':Texplore<Enter>', { silent = true })
 
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { silent = true })
+
 vim.keymap.set('n', '<leader>sh', ':terminal<Enter>:set nonumber<Enter>:set norelativenumber<Enter>', { silent = true })
