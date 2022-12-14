33159c6d6950291a2fe8698125d90d660c81851f
Michael Scott <michael.scott@smartuq.com>
2022-12-14 08:20:09 -0600

    Windows updates

--- run.cmd
+++ run.cmd
@@ -1,3 +1,5 @@
 @echo off
-del %LOCALAPPDATA%\nvim
+rmdir /S /Q %LOCALAPPDATA%\nvim
 mklink /D %LOCALAPPDATA%\nvim %CD%\nvim
+rmdir /S /Q %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+mklink /D %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState %CD%\windows-terminal
