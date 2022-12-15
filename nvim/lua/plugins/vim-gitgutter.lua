e230eec5de5be898ebcae95a612e8e8e98367ce2
Michael Scott <michael.yates.scott@gmail.com>
2022-12-15 08:45:25 -0600

    Add gitgutter keybindings

--- nvim/lua/plugins/vim-gitgutter.lua
+++ nvim/lua/plugins/vim-gitgutter.lua
@@ -1,1 +1,4 @@
 vim.o.updatetime = 100
+
+vim.keymap.set('n', '<leader>gd', ':GitGutterDiffOrig<Enter>', { silent = true })
+vim.keymap.set('n', '<leader>gh', ':GitGutterLineHighlightsToggle<Enter>', { silent = true })
