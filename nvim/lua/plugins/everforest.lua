fd5de8bd6875cbc41f64e585a51d55255834b482
Michael Scott <michael.yates.scott@gmail.com>
2022-12-16 15:26:06 -0600

    Experiment with transparency

--- nvim/lua/plugins/everforest.lua
+++ nvim/lua/plugins/everforest.lua
@@ -2,3 +2,5 @@ vim.o.background = 'dark'
 vim.g.everforest_background = 'soft'
 vim.g.everforest_better_performance = true
 vim.cmd('colorscheme everforest')
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
