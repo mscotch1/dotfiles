d82b07663ad45a4389fe5f9944916851539510d2
Michael Scott <michael.scott@smartuq.com>
2022-12-22 09:46:11 -0600

    Fix "+ register key maps, add :cnext/:cprev key maps

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -5,9 +5,9 @@ vim.keymap.set('n', '-t', ':Texplore<Enter>', { silent = true })
 
 vim.keymap.set('n', '<leader>sh', ':terminal<Enter>:set nonumber<Enter>:set norelativenumber<Enter>', { silent = true })
 
-vim.keymap.set('x', '<leader>y', '"+y')
-vim.keymap.set('x', '<leader>p', '"+p')
-vim.keymap.set('x', '<leader>P', '"+P')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p')
+vim.keymap.set({'n', 'v'}, '<leader>P', '"+P')
 
 vim.keymap.set('n', '<leader>gl', require'functions.gitlens'.blameVirtText, { silent = true })
 
@@ -15,3 +15,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set('n', '<C-Up>', ':cprev<Enter>')
+vim.keymap.set('n', '<C-Down>', ':cnext<Enter>')
