397d95a64a5913f8bc14d2c8da6e67a77871c040
Michael Scott <michael.scott@smartuq.com>
2023-02-17 10:53:29 -0600

    Fix urls in Lazy config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,78 +1,78 @@
 return {
     {
-        url = 'lewis6991/gitsigns.nvim',
+        'lewis6991/gitsigns.nvim',
         init = function()
             require('plugin-config.gitsigns')
         end
     },
     {
-        url = 'sainnhe/everforest',
+        'sainnhe/everforest',
         init = function()
             require('plugin-config.everforest')
         end
     },
     {
-        url = 'nvim-lualine/lualine.nvim',
+        'nvim-lualine/lualine.nvim',
         init = function()
             require('plugin-config.lualine')
         end
     },
     {
-        url = 'williamboman/mason.nvim',
+        'williamboman/mason.nvim',
         init = function()
             require('plugin-config.mason')
         end
     },
     {
-        url = 'preservim/nerdcommenter',
+        'preservim/nerdcommenter',
         init = function()
             require('plugin-config.nerdcommenter')
         end
     },
     {
-        url = 'hrsh7th/nvim-compe',
+        'hrsh7th/nvim-compe',
         init = function()
             require('plugin-config.nvim-compe')
         end
     },
     {
-        url = 'neovim/nvim-lspconfig',
+        'neovim/nvim-lspconfig',
         init = function()
             require('plugin-config.nvim-lspconfig')
         end
     },
     {
-        url = 'nvim-treesitter/nvim-treesitter',
+        'nvim-treesitter/nvim-treesitter',
         init = function()
             require('plugin-config.nvim-treesitter')
         end
     },
     {
-        url = 'nvim-lua/plenary.nvim',
+        'nvim-lua/plenary.nvim',
         init = function()
             require('plugin-config.plenary')
         end
     },
     {
-        url = 'nvim-telescope/telescope.nvim',
+        'nvim-telescope/telescope.nvim',
         init = function()
             require('plugin-config.telescope')
         end
     },
     {
-        url = 'ryanoasis/vim-devicons',
+        'ryanoasis/vim-devicons',
         init = function()
             require('plugin-config.vim-devicons')
         end
     },
     {
-        url = 'tpope/vim-fugitive',
+        'tpope/vim-fugitive',
         init = function()
             require('plugin-config.vim-fugitive')
         end
     },
     {
-        url = 'folke/zen-mode.nvim',
+        'folke/zen-mode.nvim',
         init = function()
             require('plugin-config.zen-mode')
         end
